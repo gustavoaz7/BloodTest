4b0ea6e664f209d118433b3df9a78bf3ecc1e5ea
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-11-11 00:11:41 +0100

    Improve loading tests

--- app/components/__tests__/Loading.test.js
+++ app/components/__tests__/Loading.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { shallow } from 'enzyme';
 import Loading from '../Loading';
+import { theme } from '../../theme';
 
 describe('Loading', () => {
   it('renders without crashing', () => {
@@ -10,5 +12,37 @@ describe('Loading', () => {
   it('matches snapshot', () => {
     const wrapper = shallow(<Loading />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('color', () => {
+    it("defaults to theme's primary", () => {
+      const wrapper = shallow(<Loading />);
+      const colorProp = wrapper.find(ActivityIndicator).props().color;
+
+      expect(colorProp).toBe(theme.primary);
+    });
+
+    it('accepts custom color', () => {
+      const wrapper = shallow(<Loading color={'black'} />);
+      const colorProp = wrapper.find(ActivityIndicator).props().color;
+
+      expect(colorProp).toBe('black');
+    });
+  });
+
+  describe('size', () => {
+    it('defaults to large', () => {
+      const wrapper = shallow(<Loading />);
+      const sizeProp = wrapper.find(ActivityIndicator).props().size;
+
+      expect(sizeProp).toBe('large');
+    });
+
+    it('accepts custom size', () => {
+      const wrapper = shallow(<Loading size={'small'} />);
+      const sizeProp = wrapper.find(ActivityIndicator).props().size;
+
+      expect(sizeProp).toBe('small');
+    });
   });
 });
