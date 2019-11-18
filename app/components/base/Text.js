18b434d12426fd597ae4b9aa6786ca81a170e6ac
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-11-19 00:46:55 +0100

    Refactor Text to properly use withTheme

--- app/components/base/Text.js
+++ app/components/base/Text.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import { Text } from 'react-native';
-import styled from 'styled-components/native';
-import { theme } from '../../theme';
+import styled, { withTheme } from 'styled-components/native';
+import PropTypes from 'prop-types';
+import { themeType } from '../../types';
 
-export default styled(Text)`
-  color: ${theme.secondary};
-  font-family: ${theme.fontFamily};
+function TextUI({ children, theme, ...rest }) {
+  return <ThemedText {...rest}>{children}</ThemedText>;
+}
+
+const ThemedText = styled(Text)`
+  color: ${({ theme }) => theme.secondary};
+  font-family: ${({ theme }) => theme.fontFamily};
 `;
+
+TextUI.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  children: PropTypes.any.isRequired,
+  theme: themeType.isRequired,
+};
+
+export default withTheme(TextUI);
