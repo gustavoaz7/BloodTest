d8cf00dc25ea6f769a86a9b1dc83d25cc8bbb745
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2019-11-19 01:54:45 +0100

    Gets IP address via env variable

--- app/services/api.js
+++ app/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { IP_ADDRESS } from 'react-native-dotenv';
 
-const BASE_URL = 'http://192.168.0.13:4000';
+const BASE_URL = `http://${IP_ADDRESS}:4000`;
 
 const fetch = path => axios.get(`${BASE_URL}/${path}`);
 
